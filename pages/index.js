15fbc0fbc67fbdeaa96c1ed54dbc6b1e35688f3e
Marvin Liu <marvin.liu@fairfaxmedia.com.au>
2017-11-08 17:29:03 +1100

    Setup the project

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/BasicLayout'
+
+const Index = () => (
+    <Layout>
+        <h1>Tag Management</h1>
+    </Layout>
+)
+
+export default Index
---
+++ pages/new.js
@@ -0,0 +1,9 @@
+import Layout from '../components/BasicLayout'
+
+const NewTag = () => (
+    <Layout>
+        <h1>Create A New Tag</h1>
+    </Layout>
+)
+
+export default NewTag
