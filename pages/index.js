717c095a54f338da62c7edc658fc6c4f8b63a371
Marvin Liu <superliuwr@gmail.com>
2017-11-08 17:36:13 +1100

    Add placeholder for search bar

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Layout from '../components/BasicLayout'
 const Index = () => (
     <Layout>
         <h1>Tag Management</h1>
+        <div>
+            TBD - Add search bar here
+        </div>
     </Layout>
 )
 
